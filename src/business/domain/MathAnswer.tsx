374d8c192fddfaf1beab6d8260e1879c29b35179
Guilhem Maruejouls <samagacegrave@gmail.com>
2023-01-24 22:13:22 +0100

    Updates

--- src/business/domain/MathAnswer.tsx
+++
@@ -1,9 +0,0 @@
-export default class MathAnswer {
-  readonly answerDate: Date;
-  readonly answer: number;
-
-  public constructor(answer: number, date: Date) {
-    this.answerDate = date;
-    this.answer = answer;
-  }
-}
